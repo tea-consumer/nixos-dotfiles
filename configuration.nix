806cf0cd753f2ebb76d74a90f7e898e67949c1f2
tea-consumer <159437626+tea-consumer@users.noreply.github.com>
2025-12-03 18:25:13 +0000

    Add files via upload

---
+++ configuration.nix
@@ -0,0 +1,165 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ config, lib, pkgs, inputs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # networking.hostName = "nixos"; # Define your hostname.
+  # Pick only one of the below networking options.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+  # Set your time zone.
+  # time.timeZone = "Europe/Amsterdam";
+  services = {
+ 	 displayManager.sddm.enable = true;
+ 	 displayManager.sddm.wayland.enable = true;
+  };
+
+  programs.hyprland = {
+	enable = true;
+	xwayland.enable = true;
+	
+  };
+
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkb.options in tty.
+  # };
+
+  # Enable the X11 windowing system.
+  # services.xserver.enable = true;
+
+
+  
+
+  # Configure keymap in X11
+  # services.xserver.xkb.layout = "us";
+  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  services.pulseaudio.enable = true;
+  services.pipewire.enable = lib.mkForce false;
+  # OR
+  # services.pipewire = {
+  #   enable = true;
+  #   pulse.enable = true;
+  # };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+ users.users.fredrik = {
+   isNormalUser = true;
+   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+   packages = with pkgs; [
+     tree
+   ];
+ };
+
+   programs.firefox.enable = true;
+
+  # List packages installed in system profile.
+  # You can use https://search.nixos.org/ to find more packages (and options).
+	programs.steam = {
+  		enable = true;
+ 		remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+  		dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  		localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+	};	
+    nixpkgs.config.allowUnfree = true;
+    environment.sessionVariables.NIXOS_OZONE_WL = "1";
+    environment.systemPackages = with pkgs; [
+     wmenu
+     wl-clipboard
+     vim
+     wget
+     kitty
+     waybar
+     git
+     rofi
+     hyprpaper
+     inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
+     pavucontrol
+     kdePackages.dolphin
+     rose-pine-hyprcursor
+     cliphist
+     kdePackages.kate
+     hyprpolkitagent
+     vscodium
+     btop
+     rose-pine-cursor
+   ];
+
+   nix.settings.experimental-features = ["nix-command" "flakes"];
+
+   fonts.packages = with pkgs; [
+    	nerd-fonts.jetbrains-mono
+    ];
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+}
+
